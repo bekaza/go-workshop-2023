436b7c33c82d7b92de30e03b65aa2f09d0ab314f
BKZ <bekaza759@gmail.com>
2023-06-08 21:32:49 +0700

    inti qr promptpay

--- demo/04-qr-poc/main.go
+++ demo/04-qr-poc/main.go
@@ -1,3 +1,11 @@
 package main
 
-func main() {}
+import "fmt"
+
+func generateQrPromptPay(promptPayID string, amount float64) string {
+	return ""
+}
+
+func main() {
+	fmt.Println("QR string -", generateQrPromptPay("0882314328", 10.0))
+}
